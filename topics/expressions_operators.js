dab75fd36a0a10a9b6852b3558c25ce80306660b
jaygraygray <jeremygray001@gmail.com>
2021-01-18 16:18:38 -0500

    wip

---
+++ topics/expressions_operators.js
@@ -0,0 +1,42 @@
+/**
+ * ## Expressions
+ * Anything that evaluates to a single value. Expressions can
+ * assign value, or simply have a value. Think of an expression 
+ * as a logical unit to help perform operations. Expressions
+ * help our applications and websites make decisions. 
+ * 
+ *      x = 7
+ * is an expression that assigns x the value of 7.
+ * 
+ *      3 + 7
+ * evaluates to 7. 
+ * 
+ * "Hello, " + "John"
+ * evaluates to a string
+ */
+
+/** ## TYPE OF EXPRESSIONS
+ * 
+ * JavaScript has 3 different types of expressions:
+ * - Arithmetic: evaluates to a number
+ * - String: evaluates to a string 
+ * - Logical: evaluates to true or flase
+ * 
+ * The semi-colon is used to terminate expressions. Optional, but can help improve readability
+ */
+
+ 2 + 2;      
+ true === false;
+ "I am " + "hungry.";
+
+/** ## ISOLATING AND COMBINING EXPRESSIONS
+ * 
+ * Parenthesis are used to define or clarify, as well as combine
+ * expressions into larger ones.
+ */
+(2+2) - (2+2);
+((2+2) - (2+2)) + 4;
+
+/** ## OPERATORS
+ * 
+ */
