873a2f6d1ade9e8e0fe8054896303a5b71127c63
Dan Morey <danjmorey@gmail.com>
2021-01-26 18:36:15 -0500

    YOLO :)

---
+++ students/daniel/drills/variables.js
@@ -0,0 +1,36 @@
+/* 
+  ~🧠~ Variable Drills ~🧠~
+  If an instruction does not tell you what to name your variable,
+  assume you can name it anything you want.
+
+  If an instruction does not tell you the type of variable 
+  to declare, use a string.
+
+  Use double quotes by default.
+
+  Include a semi-colon after all of your variable declarations;
+
+  The three keywords that can be used to declare variables are
+    - 
+    -
+    -
+*/
+
+// Declare a variable with the name of your favorite celebrity
+// and the value being their profession
+
+// Declare a variable with the string value of how much money you wish you had
+
+// Declare a variable with the value of a food.
+
+// Declare a variable with the value of an article of clothing.
+
+// Declare a boolean variable with the name amIHungry.
+
+// Declare a boolean variable with the name thisIsConfusing
+
+// Declare a number variable with the address number of your residence
+
+// Declare a variable with the name of the last movie you watched
+
+// Declare a variable with the name of a wild animal
