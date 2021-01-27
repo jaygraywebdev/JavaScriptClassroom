47c5d53ac05ee867ecdbbb9e971efa086d364efd
cwicho <caitlincasey52@gmail.com>
2021-01-26 19:27:25 -0500

    Var

--- students/caitlin/drills/objects.js
+++ students/caitlin/drills/objects.js
@@ -7,11 +7,29 @@ They help us show how things are related
 to one another.
 
 Objects are created with {braces}
+
+Made up key/value pairs.
+
+Values can be any data type in javascript
+
+Keys can be stringsgi
 */
 
 const person = {
     name: "Billy Bob",
     age: 96,
+    job: null,
+    friends: ["Diana", "Paulina", "Georgina"],
+    someRandomObject: {}//nested objects
 }
 
+//Dot Notation
 console.log(person.name)
+
+//Bracket Notation
+console.log(person["name"])
+
+//Bracket notation can be useful when you want
+//to use dynamic key names
+const keyName = "name";
+console.log{person[keyName]};
