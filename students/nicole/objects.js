aa200502305308e46f912d05b7285deca27cdd45
Nicole Gray <ngray122@gmail.com>
2021-01-26 19:07:44 -0500

    object

---
+++ students/nicole/objects.js
@@ -0,0 +1,18 @@
+/* 
+   Objects Drills
+
+   Objects are used store relational data.
+
+   They will help us show how things are related to one another
+
+   Objects are created with {braces}
+*/
+
+
+const person = {
+    name: "Billy Bob",
+    age: 96,
+}
+
+console.log(person.name);
+console.log(person{"name"});
