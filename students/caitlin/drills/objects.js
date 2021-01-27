b37e922112ffcbee59c68982d8140f5fbb665fa5
cwicho <caitlincasey52@gmail.com>
2021-01-26 19:07:53 -0500

    Object

---
+++ students/caitlin/drills/objects.js
@@ -0,0 +1,17 @@
+/*
+object drills
+
+objects are used to store relational data,
+
+They help us show how things are related
+to one another.
+
+Objects are created with {braces}
+*/
+
+const person = {
+    name: "Billy Bob",
+    age: 96,
+}
+
+console.log(person.name)
