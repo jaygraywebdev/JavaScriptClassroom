d01cc0de34583496901d8c4266be29681c4bfa0d
jaygraygray <jeremygray001@gmail.com>
2021-02-02 19:02:59 -0500

    update functions drills file

--- lab/drills/functions.js
+++ lab/drills/functions.js
@@ -13,7 +13,7 @@
     // perform crazy computation to encrypt the message!
   }                                                             /*
 
-  3. Anonymous function expression                              */
+  3. Anonymous function expression (fat arrow function)                              */
   const encryptMessage = (msg) => {
     // perform crazy computation to encrypt the message!
   }                                                             /*
@@ -21,20 +21,36 @@
   When creating functions, variables like msg are considered "parameters." 
   When we're creating functions, the variables that are
   "passed in" are called parameters as long as we're interacting
-  with him between the function's braces { }
+  with him between the function's braces { }        */
+  function doFunThing(someCrazyThing, parameterTwo) {
+    const number = someCrazyThing + parameterTwo;
+  }
+
+  /*
 
   ### Using Functions
 
   When you make a function do the thing it's supposed to, 
   you "invoke" or "call" the function. You invoke a function
-  by using parenthesis() directly() next() to() your() funciton()
+  by using parenthesis() directly() next() to() your() function()
   name()                                                        */
 
   encryptMessage("Whatever needs to be encrypted");              /*
 
   When you invoke a function, the values you "pass in"
   to the function are called "arguments." Here, the
-  argument is a string. 
+  argument is a string.                   */
+
+  const argumentOne = 1;
+  const argumentTwo = 2;
+  doFunThing(argumentOne, argumentOne);
+
+  // When we invoke functions, the variables passed in are arguments.
+
+  /*
+
+  Creating functions think parameters
+  Invoking functions, think arguments
 
   It's much more common to pass in variables as
   arguments to a function.                                  */
@@ -43,7 +59,6 @@
 
   The names of parameters and arguments do not correllate to each other.
 
-  
   https://www.freecodecamp.org/news/when-to-use-a-function-declarations-vs-a-function-expression-70f15152a0a0/
   
 */
